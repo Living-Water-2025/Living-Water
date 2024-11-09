3b986bd95c1e3ff11d10a8942a804b918c8673ee
Dan Jost <danjjost@gmail.com>
2024-11-09 13:59:32 -0600

    Add map placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import {Map} from '../components/Map';
+
 export default function Home() {
   return (
     <div className="grid justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-5xl font-thin">Living Water 2025</h1>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Content
+        <Map/>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         Footer
---
+++ components/Map.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Map: React.FC = () => {
+    return (
+        <div>
+            <p>Map Placeholder</p>
+        </div>
+    );
+};
